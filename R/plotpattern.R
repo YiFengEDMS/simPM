07433750fda450c5bdd37749d5fdace4ab5c054c
Yi Feng <yifeng94@umd.edu>
2019-07-08 18:56:39 -0400

    update plotPM: add an option to not plot wave legend

--- R/plotpattern.R
+++ R/plotpattern.R
@@ -1,4 +1,4 @@
-#' Plot the missing data patterns
+#' Plot the missing data patterns for the optimal PM design
 #' @param object The simPM object
 #' @param Time The total number of waves
 #' @param k The number of observed variables at each wave
@@ -19,20 +19,28 @@ plotPM=function(object,
                 k,
                 colbr="PRGn",
                 col=c("antiquewhite1","firebrick"),
-                row.names=T){
+                row.names=T,
+                labels=T){
   data=object$opt.pattern
   if (row.names==T){
-  row.names(data)=paste0(paste0("pt",1:nrow(data),":",sep=""),"n=",object$opt.ns)
+    row.names(data)=paste0(paste0("pt",1:nrow(data),":",sep=""),"n=",object$opt.ns)
   }
   annotation=data.frame(Wave=factor(rep(c(1:Time),each=k), labels = paste0("W",1:Time)))
   rownames(annotation) <- colnames(data)
   mat_colors <- list(Wave = brewer.pal(Time, colbr))
   names(mat_colors$Wave) <- paste0("W",1:Time)
-
-  pheatmap(data, scale = "none",col=col,
-                       cluster_rows = F, cluster_cols = F,legend=T,legend_breaks=c(0,1),legend_labels=c("complete","missing"),fontsize_row = 14, fontsize_col=14,fontsize=14,drop_levels=T,annotation = annotation,annotation_colors = mat_colors,angle_col=45)
-
+  
+  if (labels==T){
+    pheatmap(data, scale = "none",col=col,
+             cluster_rows = F, cluster_cols = F,legend=T,legend_breaks=c(0,1),legend_labels=c("complete","missing"),fontsize_row = 14, fontsize_col=14,fontsize=14,drop_levels=T,annotation = annotation,annotation_colors = mat_colors,angle_col=45)
+  }
+  
+  if (labels==F){
+    pheatmap(data, scale = "none",col=col,
+             cluster_rows = F, cluster_cols = F,legend=T,legend_breaks=c(0,1),legend_labels=c("complete","missing"),fontsize_row = 14, fontsize_col=14,fontsize=14,drop_levels=T,angle_col=45)
+  }
 }
+
 
 
 
