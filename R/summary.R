0012aac61225d92a256b46ebdb1f11ccead1344e
Yi Feng <yifeng94@umd.edu>
2019-12-10 20:41:41 -0500

    update the summary function.

--- R/summary.R
+++ R/summary.R
@@ -16,21 +16,40 @@
 
 summary.simpm <- function(object) {
   
-  focal.param <- object$misc$focal.param
-  opt.powers <- summaryParam(object$opt.output)[focal.param, ]
+  if (length(object) == 2) {
+    focal.param <- object[[1]]$misc$focal.param
+    opt.powers <- summaryParam(object[[1]]$opt.output)[focal.param, ]
+    
+    print("=================results summary================")
+    print(object[[1]]$results)
+    print("=================Optimal design=================")
+    print(object[[1]]$opt.design)
+    print("=================Optimal design for focal parameters=================")
+    print(opt.powers)
+    print("=================Optimal patterns===============")
+    print(object[[1]]$opt.pattern)
+    print("=================Optimal probs==================")
+    print(object[[1]]$opt.probs)
+    print("=================Optimal ns====================")
+    print(object[[1]]$opt.ns)
+    
+  }else{
+    focal.param <- object$misc$focal.param
+    opt.powers <- summaryParam(object$opt.output)[focal.param, ]
+    
+    print("=================results summary================")
+    print(object$results)
+    print("=================Optimal design=================")
+    print(object$opt.design)
+    print("=================Optimal design for focal parameters=================")
+    print(opt.powers)
+    print("=================Optimal patterns===============")
+    print(object$opt.pattern)
+    print("=================Optimal probs==================")
+    print(object$opt.probs)
+    print("=================Optimal ns====================")
+    print(object$opt.ns)
+  }  
   
-  
-  print("=================results summary================")
-  print(object$results)
-  print("=================Optimal design=================")
-  print(object$opt.design)
-  print("=================Optimal design for focal parameters=================")
-  print(opt.powers)
-  print("=================Optimal patterns===============")
-  print(object$opt.pattern)
-  print("=================Optimal probs==================")
-  print(object$opt.probs)
-  print("=================Optimal ns====================")
-  print(object$opt.ns)
 }
 
